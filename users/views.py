637dc0187f3d7dfa63e8936c80a72e8ce137e655
Yury <109030530+Yury08@users.noreply.github.com>
2023-02-15 18:45:15 +0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,11 +1,48 @@
+from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm, UserUpdateForm, ProfileImagesForm
 from django.contrib.auth.decorators import login_required
-
 from .models import Profile
+from .forms import (UserRegisterForm,
+                    UserUpdateForm,
+                    ProfileImagesForm,
+                    UserLoginForm)
+from django.views.generic.base import (
+    TemplateResponseMixin, # Добавит формирование html шаблона и вернёт его в качестве ответа на запрос
+    View # Базовый класс для обработчиков django
+)
+from django.contrib.auth import (
+    authenticate,
+    login
+)
+
+
+class Login(LoginView, TemplateResponseMixin):
+    template_name = 'users/user.html'
+
+    def get(self, request, *args, **kwargs):
+        form = UserLoginForm()
+        return self.render_to_response({'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('films:main')
+                else:
+                    messages.error(request, 'Логин или пароль не корректны')
+                    return redirect('user')
+            return redirect('films:main')
+        return self.render_to_response({'form': form})
 
 
 def register(request):
@@ -13,11 +50,11 @@ def register(request):
         form = UserRegisterForm(request.POST)  # Хранятся все данные полученные от позователя
         if form.is_valid():  # Проверка данных полученных о пользователя
             form.save()  # Регестрируем пользоваеля в базе данных
-            return redirect('home')  # Перебрасываем порльзователя на главную страницу после регистрации
+            return redirect('users:user')  # Перебрасываем порльзователя на главную страницу после регистрации
     else:
         form = UserRegisterForm()
 
-    return render(request, 'users/register.html', {'title': 'Страница регистрации', 'form': form})
+    return render(request, 'users/register.html', {'form': form})
 
 
 @login_required
@@ -46,6 +83,6 @@ def profile(request):
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-        return redirect('profile')
+        return redirect('users:user')
     else:
         instance.profile.save()
